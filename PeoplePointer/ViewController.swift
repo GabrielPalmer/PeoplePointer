5566de3f0b59c825aa4e54faa99d35983cf18b84
Gabriel Palmer <gabepalmer@gabriels-macbook-air.local>
2018-09-13 08:07:31 -0600

    Initial Commit

---
+++ PeoplePointer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeoplePointer
+//
+//  Created by Gabriel Blaine Palmer on 9/13/18.
+//  Copyright © 2018 Gabriel Blaine Palmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
